d97439c981c85b5f483139bdfd6b97a8cf991ab0
hannah hayward <hannahhayward97@gmail.com>
2021-07-20 13:30:18 -0600

    crossing setup t's and dotting i's

--- angieslist.client/src/env.js
+++ angieslist.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'http://localhost:5001' : ''
+export const domain = 'hsquared.us.auth0.com'
+export const audience = 'bug-log.com'
+export const clientId = '6836x1G4rTlqcdmNdXaL1NXxDsnKPOh2'
